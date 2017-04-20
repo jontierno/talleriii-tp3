2cf4f0652cb17652055fe35c0caa1bbec935ea02
Jonathan <jonathan.tierno@gmail.com>
2017-04-19 22:42:08 -0300

    limpieza

--- app/mainapp.js
+++ app/mainapp.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-var matchApp = angular.module('ErrorsLog', [
+var matchApp = angular.module('MatchApp', [
   'ngRoute','angular.filter', 'mgcrea.ngStrap'
 ]);
 matchApp.config(['$routeProvider',
@@ -9,12 +9,12 @@ matchApp.config(['$routeProvider',
     $routeProvider.
       when('/applications', {
         templateUrl: 'applications.html',
-
+        controller: 'ApplicationController'
       }).
       when('/functions', {
         templateUrl: 'functions.html',
-        
-      })
+        controller: 'FunctionsController'
+      }).
       otherwise({
         redirectTo: '/applications'
       });
