f27242520a33d7e044c3483bf75a6a9b47df6e58
Jonathan <jonathan.tierno@gmail.com>
2017-04-22 15:02:29 -0300

    paginación de las dos consultas

--- app/controller.js
+++ app/controller.js
@@ -3,27 +3,73 @@ var app= angular.module('ErrosLog');
 
 app.controller("ApplicationController", function($scope, $http, $timeout){
 	var self = this;
-	$http({
-    	url: "rest/applications", 
-    	method: "GET",
- 	}).then(function(response){
-		self.applications = response.data.result;
-		self.more = response.data.more;
-		self.cursor = response.data.cursor;
-	});		
+	
+	
+
+ 	self.reload = function (next){
+ 		var params = {};
+ 		if(next){
+ 			params.next = next;
+ 		}
+ 		var append= next; 
+ 		$http({
+	    	url: "rest/applications", 
+	    	method: "GET",
+	    	params: params
+	 	}).then(function(response){
+	 		console.log(append)
+	 		if(append){
+				self.applications = self.applications.concat(response.data.result)
+	 		} else {
+	 			self.applications = response.data.result;
+	 		}
+			
+			self.more = response.data.more;
+			self.next = response.data.next;
+		});	
+
+ 	};
+
+ 	self.loadMore = function (){
+ 		console.log(self.next)
+ 		self.reload(self.next);
+ 	};
+ 	self.reload();
+
 
 });
 app.controller("FunctionsController", function($scope, $http, $location){
 	var self = this;
-	$http({
-    	url: "rest/functions", 
-    	method: "GET",
-    	params: {time: 6}
- 	}).then(function(response){
-		self.functions = response.data.result;
-		self.more = response.data.more;
-		self.cursor = response.data.cursor;
-	});		
+ 	self.reload = function (next){
+ 		var params = {time: 12};
+ 		if(next){
+ 			params.next = next;
+ 		}
+ 		var append= next; 
+ 		$http({
+	    	url: "rest/functions", 
+	    	method: "GET",
+	    	params: params
+	 	}).then(function(response){
+	 		console.log(append)
+	 		if(append){
+				self.functions = self.functions.concat(response.data.result)
+	 		} else {
+	 			self.functions = response.data.result;
+	 		}
+			
+			self.more = response.data.more;
+			self.next = response.data.next;
+		});	
+
+ 	};
+
+ 	self.loadMore = function (){
+ 		console.log(self.next)
+ 		self.reload(self.next);
+ 	};	
+
+ 	self.reload();
 });
 
 
